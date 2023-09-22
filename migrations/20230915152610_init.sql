66a840b56aba45d65772e5376485d2ae2a3c3547
Jonathan R. Warden <john.warden@gmail.com>
2023-09-22 17:05:07 +0200

    Add some sample tweets

--- migrations/20230915152610_init.sql
+++ migrations/20230915152610_init.sql
@@ -108,7 +108,21 @@ group by 1,2;
 
 insert into posts(id, content, parent_id, question_id) values (1, "So, pregnant people can’t cross state lines to get abortions but guys like Kyle Rittenhouse can cross state lines to murder people. Seems fair.", null, null);
 insert into posts(id, content, parent_id, question_id) values (2, "Kyle Rittenhouse was acquitted of murder charges. Clear video evidence showed he acted in self defense.", 1, null);
-insert into posts(id, content, parent_id, question_id) values (3, "That trial was a sham. All the jurors where white.", 2, null);
+insert into posts(id, content, parent_id, question_id) values (3, "That trial was a sham. They were never going to convict.", 2, null);
+
+
+
+insert into posts(id, content, parent_id, question_id) values (4, "Sudafed, Benadryl and most decongestants don’t work: FDA advisory panel https://trib.al/sJmOJBP", null, null);
+insert into posts(id, content, parent_id, question_id) values (5, "This is misleading. Regular Benadryl is an antihistamine; it is not a decongestant. There is a Benadryl branded product that is impacted.
+https://www.nbcnews.com/news/amp/rcna104424", 4, null);
+
+
+
+insert into posts(id, content, parent_id, question_id) values (6, "Right now, real wages for the average American worker is higher than it was before the pandemic, with lower wage workers seeing the largest gains.
+
+  That's Bidenomics.", null, null);
+insert into posts(id, content, parent_id, question_id) values (7, "The tweet’s claim about real wages contains a factual error. On 3/15/20 when US COVID lockdowns began real wages adjusted for inflation (AFI) were $11.15. As of 7/16/23 real wages AFI are $11.05. Real wages AFI remain lower (not higher) than before the pandemic.", 6, null);
+
 
 
 
